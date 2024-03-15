39ae93d38f12b7fd2a65aa210c28423c9e27bb73
root <root@Nour>
2024-03-15 02:34:33 +0200

    ${com}

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- show the description of a table
-SELECT column_name, column_type, character_maximum_length, is_nullable, column_default
-FROM information_schema.columns
-WHERE table_schema = '$1' AND table_name = 'first_table';
+SHOW CREATE TABLE first_table;
