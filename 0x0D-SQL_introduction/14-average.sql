617bf7a38e1e687198e768d1cec5b996e1186201
root <root@Nour>
2024-03-16 02:27:11 +0200

    ${com}

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
--- computes the score average of all records in the table second_table of the database hbtn_0c_0
-SELECT AVG(score) FROM second_table;
+-- compute the score average of all records in the table second_table of the database hbtn_0c_0
+SELECT AVG(score) FROM second_table AS average;
