c5b1de265e9488fe2ce2690dcd5d40949be30528
root <root@Nour>
2024-03-18 00:07:47 +0200

    ${com}

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- display the average temperature (Fahrenheit) by city ordered by temperature (descending)
-SELECT city, AVG((value * 9/5) + 32) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-GROUP BY city, value
-ORDER BY avg_temp;
+GROUP BY city
+ORDER BY avg_temp DESC;
