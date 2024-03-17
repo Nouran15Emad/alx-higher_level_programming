f7765d242ed312c189f5a0149b230f6776b3de26
root <root@Nour>
2024-03-17 23:53:50 +0200

    ${com}

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,17 @@
--- Change the database collation
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+-- Convert the database to UTF8
+ALTER DATABASE
+    hbtn_0c_0
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
-ALTER TABLE first_table CONVERT
-TO CHARACTER SET utf8mb4 
-COLLATE utf8mb4_unicode_ci;
+ALTER TABLE
+    first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table  
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE
+    first_table
+    CHANGE name name
+    VARCHAR(256)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
