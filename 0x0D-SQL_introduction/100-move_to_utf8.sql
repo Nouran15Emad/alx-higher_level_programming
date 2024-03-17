8c6505fab394e720beda723331ef4ead21e2e713
root <root@Nour>
2024-03-17 23:35:27 +0200

    ${com}

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,12 @@
 -- Change the database collation
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
--- Change the table collation
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT
+TO CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
 
--- Change the field collation
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table 
+MODIFY name VARCHAR(256) 
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
