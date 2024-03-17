c39a195ccf3dbb876f85f2c9823db34d5dc44fd6
root <root@Nour>
2024-03-17 23:41:40 +0200

    ${com}

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,6 +7,5 @@ ALTER TABLE first_table CONVERT
 TO CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table 
-MODIFY name VARCHAR(256) 
+ALTER TABLE first_table  
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
