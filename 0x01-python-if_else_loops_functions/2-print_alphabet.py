a190788840afa330e098f3e2dddd04f002ed6f71
Nouran15Emad <nouran1542001@gmail.com>
2024-09-05 16:39:00 +0300

    ${com}

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for letter in range(ord('a'), ord('z') + 1):
+    print(chr(letter), end="")
+# ord is a function that convert the character to its Ascii code ex: ord('a') = > 
