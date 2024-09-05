6dec9e4a80d292789f4bc924c655f0dbe1d52751
Nouran15Emad <nouran1542001@gmail.com>
2024-09-05 16:51:42 +0300

    ${com}

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-for letter in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(letter)), end="")
+#for letter in range(ord('a'), ord('z') + 1):
+    #print("{}".format(chr(letter)), end="")
+
 # ord is a function that convert the character to its Ascii code
 # ex: ord('a') = > 97
 # and that is because for in range work on numbers
 # so we need to convert letters to numbers then reconvert them to letters
 # using chr function  chr(97)  = >
 #  'a' when print
+print(''.join(chr(i) for i in range(ord('a'), ord('z') + 1)), end="")
